a1c10e52165faf720fc6265a63b9d130c800903e
mkirby1995 <31753757+mkirby1995@users.noreply.github.com>
2018-01-09 22:39:07 -0600

    Create ex6.py

---
+++ ex6.py
@@ -0,0 +1,25 @@
+# Exercise 6: Strings and Text
+# 6 of 52, 11.5%
+
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}."
+
+print (x)
+print (y)
+
+print (f"I said: {x}")
+print (f"I also said: '{y}'")
+
+hilrious = false
+joke_evaluation = "Isnt that joke so funny?! {}"
+
+print (joke_evaluation .format(hilarious))
+
+w = "this is the left side of..."
+e = "a string with a right side."
+
+print (w+e)
