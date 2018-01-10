3e5a08919b2da0f58ccdd3ae6ae61c8659e23f46
mkirby1995 <31753757+mkirby1995@users.noreply.github.com>
2018-01-09 22:24:17 -0600

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,21 @@
+# Exercise 5: More Variables and Printing
+# set 5 of 52, 9.6%
+
+my_name = 'Mathew D. kirby'
+my_age = 22 # not a lie
+my_height = 72 # inches
+my weight = 220 # lbs
+my_eyes = 'green'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print (f"Lets talk about {my_name}.")
+print (f"He's {my_height} inches tall.")
+print (f"He's {my_weight} pounds heavy.")
+print (f"Actually, thats not too heavy.")
+print (f"He's got {my_eyes} eyes, and {my_hair} hair.")
+print (f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = my_age + my_height + my_weight
+print (f"If I add {my_eyes}, {my_height}, and {my_weight}, I get {total}.")
