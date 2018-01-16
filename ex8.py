92510ebee47f99217d38ef3108741a7b3c1140be
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-01-15 18:05:02 -0600

    Create ex8.py

---
+++ ex8.py
@@ -0,0 +1,12 @@
+formatter = "{} {} {} {}"
+
+print (formatter.format(1,2,3,4))
+print (formatter.format("one" "two" "three" "four"))
+print (formatter.format(true,false,false,true))
+print (format.formatter(formatter, formatter, formatter, formatter))
+print (formatter.format (
+  "try your",
+    "own text here",
+    "maybe a poem",
+    "or a song about fear"
+    ))
