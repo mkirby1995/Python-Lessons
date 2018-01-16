e2ebda690908af7a885d3c1fa6365ec789cb5cec
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-01-15 20:55:44 -0600

    Create ex10.py

---
+++ ex10.py
@@ -0,0 +1,17 @@
+# Exercise 10: What Was That? 10 of 52, 19%
+
+tabby_cat = "\I'm tabbed in."
+persian_cat = ""I'm split\non a new line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+Ill do a lis:
+\t* Cat food
+\t* fishies
+\t* Catnip\n\t* Grass
+"""
+
+print (tabby_cat)
+print (persian_cat)
+print (backslash_cat)
+print (fat_cat)
