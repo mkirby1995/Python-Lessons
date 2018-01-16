6cab9d932232bb459a94af6f594bef64ca77f929
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-01-15 18:12:53 -0600

    Create ex9.py

---
+++ ex9.py
@@ -0,0 +1,18 @@
+# Exercise 9: Printing, Printing, Printing. 9 of 52, 17%
+
+# here is some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApril\nMay\nJun\nJul\nAug"
+
+print ("Here are the days:", days)
+print ("Here are the months:", months)
+
+print ("""
+There's something going on here.
+with th three double quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+#no spaces i between the thee double quotes
