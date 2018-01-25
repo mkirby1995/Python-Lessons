4a7019168f8bbeeb4919be780b8577c3e280206e
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-01-25 11:35:03 -0600

    Create ex13

---
+++ ex13.py
@@ -0,0 +1,13 @@
+from sys import argv
+# read the wyss section for how to run this 
+script, first, second, third = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your first variable is:", third)
+
+#line 1 is import
+# argv = argument variable
+# line 3 unpacks argv and assigns it to four variables
+#features = modules
