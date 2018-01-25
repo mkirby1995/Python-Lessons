9eeace11d0f733ec8ad3a6050b99f0399c8f0ba6
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-01-25 09:31:01 -0600

    Create ex12.py

---
+++ ex12.py
@@ -0,0 +1,10 @@
+#Excercise 12: Prompting People
+# 12 of 52, 23%
+
+age = input(("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print (f"so, you're {age} old, {height} tall and {weight} heavy.")
+
+#what does pydoc do?
