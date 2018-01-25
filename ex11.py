5836fe3ca6c3c87cc8ba754d4ab5d3e109b9cdc6
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-01-25 09:13:09 -0600

    Create ex11.py

---
+++ ex11.py
@@ -0,0 +1,13 @@
+#Exercise 11: Asking Questions 
+# 11 of 52 21%
+
+print ("How old are you?", end= ' ')
+age = input ()
+print ("How tall are you?", end = ' ')
+height = input ()
+print ("How much do you weigh?, end= ' ')
+weight = input ()
+
+print (f"So, you're {age} old, {height} tall, and {weight} heavy.")
+#find out what input does
+#write another form
