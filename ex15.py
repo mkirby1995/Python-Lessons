ea44125585e9d696cea1e97ca050b5836826ebfd
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-01-25 18:55:48 -0600

    Create ex15.py

---
+++ ex15.py
@@ -0,0 +1,17 @@
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print(f"Here's your file {filename}:")
+print(txt.read())
+
+print("Type the filename again:")
+file_again = input("> ")
+
+txt_again = open(file_again)
+
+print(txt_again.read())
+
+#python3.6 ex15.py ex15_sample.txt
