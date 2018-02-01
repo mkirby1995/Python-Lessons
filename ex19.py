f90ba1ba6809fa8646f11366ab2ac66a113b3a96
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-02-01 11:37:56 -0600

    Create ex19.py

---
+++ ex19.py
@@ -0,0 +1,6 @@
+# Exercise 19: Functions and Variables
+# 19 of 52,
+
+def cheese_and_crackers(cheese)count, boxes_of_crackers):
+  print(f"you have {cheese_count} cheese!")
+  print(f"You have {boxes_of_crackers} boxes of crackers!")cfewffferwf
