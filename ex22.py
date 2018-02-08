bf15540a8665b47d2e5d60967b7effc878ae1846
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-02-08 11:19:27 -0600

    Create ex22.py

---
+++ ex22.py
@@ -0,0 +1,37 @@
+# Exercise 22, What do you know so far
+# 22 of 52
+
+# print - use print("") to print out a string
+# # - cancels out anything after the # 
+# + - adds 
+# - - subtracts
+# / - divides
+# * - multiplyies
+# % - percentage
+# < - less than
+# > - greater than
+# <= less than equal
+# >= greater than equal
+# Variables - 
+# (f" - 
+# {} - 
+# end1 - 
+# ='' - 
+# formatter - 
+# formatter.format - 
+# \n - 
+# \t - 
+# """ - 
+# input - 
+# from sys import argv - 
+# script - 
+# string - 
+# .read - 
+# target = open - 
+# target.turncate - 
+# out_file and in_file - 
+# .close - 
+# def - 
+# *args - 
+# f.seek - 
+# rewind - 
