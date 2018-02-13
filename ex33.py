c5e6587173c2fd198aa2ca770f79ee25d3754117
Mathew Kirby <31753757+mkirby1995@users.noreply.github.com>
2018-02-13 17:28:57 -0600

    Create ex33.py

---
+++ ex33.py
@@ -0,0 +1,21 @@
+#Exercise 33: While Loops
+# 33 of 52,
+
+i = 0
+numbes = []
+
+while i < 6:
+  print(f"At the top i is [i]")
+  numbers.append(i)
+  
+  i = i + 1
+  print ("Numbers now: ", numbers)
+  print(f"At the bottom i is {i}')
+  
+  
+print("The numbers: ")
+
+for num in numbers:
+  print(num)
+  
+#python3.6 ex33.py
